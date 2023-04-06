c1db1134de5f55d0253b4e0ff6c4487dde445eac
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 20:11:40 +0200

    Completed migration of repo

---
+++ battleships/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <HomePage />
+  <BuildLayout />
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+import BuildLayout from "@/components/BuildLayout";
+import DataLayout from './data/DataLayout';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    BuildLayout,
+    HomePage
+  },
+  data(){
+    return{
+      DataLayout
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ battleships/src/components/BuildLayout.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>hello</h1>
+
+  <div id="container">
+    <div class="grid-item">Test</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BuildLayout",
+  data(){
+    return{
+      test:0,
+    }
+  },
+  methods:{
+    LayoutJs(){
+      const container = document.getElementById("container");
+
+      function makeRows() {
+        for ( let c = 0; c < (7 * 7); c++) {
+          let cell = document.createElement("div");
+          cell.innerText = (c + 1);
+          container.appendChild(cell).className = "grid-item";
+        }
+      }
+      makeRows();
+    }
+  }
+}
+</script>
+
+<style scoped>
+:root {
+  --grid-cols: 7;
+  --grid-rows: 7;
+}
+
+#container {
+  display: grid;
+  grid-gap: 1em;
+  grid-template-rows: repeat(var(--grid-rows), 1fr);
+  grid-template-columns: repeat(var(--grid-cols), 1fr);
+}
+
+.grid-item {
+  padding: 1em;
+  border: 1px solid #ddd;
+  text-align: center;
+}
+</style>
